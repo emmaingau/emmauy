2dda7d083b7c50ab3fad90df24dc52934ed83794
joaquings <44094396+joaquings@users.noreply.github.com>
2019-09-02 23:24:57 -0300

    WeGoinLiveBaby

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-     jQuery('#clock').countdown('2018/10/19',function(event){
+     jQuery('#clock').countdown('2019/11/05 14:00',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
